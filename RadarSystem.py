3a2f6c66f9226d353a417aae402fb16fb0a2516f
575743278 <575743278@qq.com>
2024-05-01 17:49:28 +0100

    modify radar system

--- RadarSystem.py
+++ RadarSystem.py
@@ -1,4 +1,6 @@
 import math
+import threading
+import time
 class RadarSystem:
     """A class control all radars to make them detect satellite positions periodically
      Predictor can get the informations by visiting radars list"""
@@ -10,10 +12,10 @@ class RadarSystem:
         self.timer = None
 
     def check_los(self, sat_pos):
-        global current_time
-        current_time = current_time + 1
+        print('check',self.current_time)
+        self.current_time += 1
         for radar in self.radars:
-            radar.line_of_sight(sat_pos, self.earth.ellipse_equation,current_time)
+            radar.line_of_sight(sat_pos, self.earth.ellipse_equation,self.current_time)
 
     def start_timer(self, interval=5):
         """Start a periodic timer that checks LOS every 'interval' seconds."""
@@ -36,12 +38,12 @@ class RadarSystem:
 """ TODO :
 Init the positions of the radors, they should be on the surface of the earth.
 """
-radars = [Radar([0, 0, 0]), Radar([1000, 1000, 1000])]
+radars = [Radar(np.array([[0], [0], [0]])), Radar(np.array([[0], [0], [0]]))]
 radar_system = RadarSystem(radars, Earth())
 """ TODO :
 change to real time interval
 """
-radar_system.start_timer(10)
+radar_system.start_timer(1)
 
 # Keep the program running to allow timer to trigger
 try:
