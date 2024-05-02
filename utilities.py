7cda2bf7eba35943cfdd53917ecaf3b4c75e61e5
genericuser101 <stepzhikharev@gmail.com>
2024-05-02 14:08:57 +0100

    unit testing

--- utilities.py
+++ utilities.py
@@ -10,14 +10,17 @@ def c_to_p(state):
     dims = np.size(state)
 
     if dims == 3:
-        x = state[0], y = state[1], z = state[2]
+        x = state[0]
+        y = state[1]
+        z = state[2]
         rho = np.sqrt(x**2 + y**2 + z**2)  # Radial distance
         theta = np.arccos(z / rho)          # Inclination angle (polar angle) from the z-axis
         phi = np.arctan2(y, x)              # Azimuthal angle from the x-axis
         polar_state = np.array([[rho], [theta], [phi]])
 
     elif dims == 2:
-        x = state[0], y = state[1]
+        x = state[0]
+        y = state[1]
         rho = math.sqrt(x**2 + y**2)
         theta = math.atan2(y, x) 
         polar_state = np.array([[rho], [theta]])
@@ -32,14 +35,17 @@ def p_to_c(state):
     dims = np.size(state)
 
     if dims == 3:
-        rho = state[0], theta = state[1], phi = state[2]
+        rho = state[0]
+        theta = state[1]
+        phi = state[2]
         x = rho * math.sin(phi) * math.cos(theta)
         y = rho * math.sin(phi) * math.sin(theta)
         z = rho * math.cos(phi)
         cartesian_state = np.array([[x], [y], [z]])
 
     elif dims == 2:
-        rho = state[0], theta = state[1]
+        rho = state[0]
+        theta = state[1]
         x = rho * math.cos(theta)
         y = rho * math.sin(theta)
         cartesian_state = np.array([[x], [y]])
