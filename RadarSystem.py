8e2eadd903639ff645da8b5dcfb7b7e51a6499df
575743278 <575743278@qq.com>
2024-05-03 20:47:36 +0100

    Modify radar system

--- RadarSystem.py
+++ RadarSystem.py
@@ -3,57 +3,36 @@ import threading
 import time
 import numpy as np
 from decorators import singleton
-
+from Radar import Radar
+from Earth import Earth
 
 @singleton
 class RadarSystem:
     """A class control all radars to make them detect satellite positions periodically
      Predictor can get the informations by visiting radars list"""
-    current_time = 0
 
     def __init__(self, radars, earth):
         self.radars = radars
         self.earth = earth
         self.timer = None
 
-    def check_los(self, sat_pos):
-        print('check', self.current_time)
-        self.current_time += 1
+    def try_detect_satellite(self, sat_pos,current_time):
         for radar in self.radars:
-            radar.line_of_sight(sat_pos, self.earth.ellipse_equation, self.current_time)
-
-    def start_timer(self, interval=5):
-        """Start a periodic timer that checks LOS every 'interval' seconds."""
-        self.timer = threading.Timer(interval, self.run_timer)
-        self.timer.start()
-
-    def run_timer(self):
-        """Handle the timer's event: check LOS and restart the timer."""
-        """ TODO :
-        True satellite positions need to be given, once the simulator finished """
-        self.check_los(np.array([[1000000], [math.pi], [math.pi]]))
-        self.start_timer()  # Restart the timer for the next check
-
-    def stop_timer(self):
-        """Stop the periodic timer."""
-        if self.timer:
-            self.timer.cancel()
+            find = radar.line_of_sight(sat_pos, self.earth.ellipse_equation)
+            if find:
+                radar.detect_satellite_pos(sat_pos, current_time)
 
 
 """ TODO :
 Init the positions of the radors, they should be on the surface of the earth.
 """
-radars = [radar(np.array([[0], [0], [0]])), radar(np.array([[0], [0], [0]]))]
-radar_system = RadarSystem(radars, earth())
-""" TODO :
-change to real time interval
-"""
-radar_system.start_timer(1)
+radars = [Radar(np.array([[0], [0], [0]])), Radar(np.array([[0], [0], [0]]))]
+radar_system = RadarSystem(radars, Earth())
 
-# Keep the program running to allow timer to trigger
-# try:
-#     while True:
-#         time.sleep(1)
-# except KeyboardInterrupt:
-#     print("Stopping radar system.")
-#     radar_system.stop_timer()
+
+''' Test code'''
+current_time = 1
+for i in range(1000):
+    radar_system.try_detect_satellite(np.array([[1000000], [math.pi], [math.pi]]), current_time)
+    current_time = current_time + 1
+
