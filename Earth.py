e0ec816d89c020a8070753d9304f870cd3de3882
575743278 <575743278@qq.com>
2024-05-03 20:41:43 +0100

    Fix  TypeError: ufunc 'bitwise_xor' not supported

--- Earth.py
+++ Earth.py
@@ -30,7 +30,8 @@ class Earth():
         Returns:
             float: _description_
         """
-        return (x^2)/(self.re^2) + (y^2)/(self.rp^2) + (z^2)/(self.re^2) - 1 
+        return (x**2)/(self.re**2) + (y**2)/(self.rp**2) + (z**2)/(self.re**2) - 1
+
 
     def distane_to_surface(self, state, tolerance = 1e-6, max_iterations = 1000):
         """Finds the minimum distance between the specified point and the ellipse
