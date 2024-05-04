dee851e4d45c20eb6a118cee6d8b6f99393de3c0
575743278 <575743278@qq.com>
2024-05-04 14:40:00 +0100

    radar update position, modify noise level

--- Radar.py
+++ Radar.py
@@ -1,14 +1,17 @@
 import numpy as np
 import math
 import utilities as ut
+from config import config
 
 
 class Radar():
+    frequency = config['radar']['frequency']
+
     def __init__(self, position):
         self.position = position
-        self.last_ping = 0 
+        self.last_ping = 0
         self.sate_pos_detected = None
-    
+
     # If the radar can detect the satellite, update sat position and last ping.
     def line_of_sight(self, Sat_Pos, Earth_Eqn):
         '''
@@ -22,39 +25,41 @@ class Radar():
 
         v0 = x - p
         mag = np.sqrt(v0.dot(v0.T))
-        v0 = v0/mag
+        v0 = v0 / mag
 
         #Checks if the line from dish to satellite goes into Earth.
-        for t in range(10)[1:]: #decrease if working for less comp time 
-            LOS_p = p + v0*t
+        for t in range(10)[1:]:  #decrease if working for less comp time
+            LOS_p = p + v0 * t
             if Earth_Eqn(LOS_p[0], LOS_p[1], LOS_p[2]) < 0:
                 return False
 
         # If no LOS break, return True
         return True
 
-    def detect_satellite_pos(self,Sat_Pos, current_time):
-        """
-        TODO This frequency should be read from config.json
-        """
-        frequency = 10
-        if current_time - self.last_ping > frequency:
+    def detect_satellite_pos(self, Sat_Pos, current_time):
+        if current_time - self.last_ping > self.frequency:
             sate_pos_detected = add_noise(Sat_Pos)
             self.last_ping = current_time
             """
             TODO  Send broadcast to Predictor 
             """
             print(sate_pos_detected)
-        
 
-def add_noise(position, noise_level=1.0):
+
+def add_noise(position):
     """
-    Adds Gaussian noise to a satellite position.
+    Adds Gaussian noise to a satellite position with different noise levels for each dimension.
 
     :param position: Original position of the satellite (numpy array or list).
-    :param noise_level: Standard deviation of the Gaussian noise.
+    :param noise_levels: List of standard deviations of the Gaussian noise for each dimension.
     :return: Noisy position.
     """
-    noisy_position = np.array(position) + np.random.normal(0, noise_level, size=np.shape(position))
-    return noisy_position
+    if len(position) == 2:
+        noise_levels = np.array([config['radar']['noise']['rho'],config['radar']['noise']['theta']])
+    elif len(position) == 3:
+        noise_levels = np.array([config['radar']['noise']['rho'],config['radar']['noise']['theta'],config['radar']['noise']['phi']])
+    if len(position) != len(noise_levels):
+        raise ValueError("The length of noise_levels must match the number of dimensions in position.")
 
+    noisy_position = np.array(position) + np.random.normal(0, noise_levels, size=np.shape(position))
+    return noisy_position
--- RadarSystem.py
+++ RadarSystem.py
@@ -1,8 +1,6 @@
 import math
-import threading
-import time
 import numpy as np
-
+from config import config
 import utilities
 from decorators import singleton
 from Radar import Radar
@@ -11,13 +9,28 @@ from Earth import Earth
 
 @singleton
 class RadarSystem:
+    radar_dt = config['sim_config']['dt']['radar_los']
+    # earth_angular_velocity = config['earth']['angular_velocity']
+    earth_angular_velocity = 2 * math.pi / 86400
+    angular_change = radar_dt * earth_angular_velocity
     """A class control all radars to make them detect satellite positions periodically
      Predictor can get the informations by visiting radars list"""
 
-    def __init__(self, radars, earth):
-        self.radars = radars
+    def __init__(self, counts, earth):
         self.earth = earth
-        self.timer = None
+        self.counts = counts
+        self.init_radar_positions()
+
+    def init_radar_positions(self):
+        points = random_points_on_ellipse(Earth(), self.counts)
+        # print(points) # check are these radar on the surface of the earth
+        # c_pos = utilities.p_to_c(points[0])
+        # print(Earth().ellipse_equation(c_pos[0], c_pos[1], c_pos[2]) )
+        radars = []
+        for point in points:
+            radar = Radar(point)
+            radars.append(radar)
+        self.radars = radars
 
     def try_detect_satellite(self, sat_pos, current_time):
         for radar in self.radars:
@@ -25,6 +38,10 @@ class RadarSystem:
             if find:
                 radar.detect_satellite_pos(sat_pos, current_time)
 
+    def update_radar_positions(self):
+        for item in self.radars:
+            item.position[1] = (item.position[1] + self.angular_change) % (2 * np.pi)
+            # print(item.position)
 
 def random_points_on_ellipse(self, num_points):
     points = []
@@ -42,18 +59,10 @@ def random_points_on_ellipse(self, num_points):
     return points
 
 
-points = random_points_on_ellipse(Earth(), 10)
-# print(points) # check are these radar on the surface of the earth
-# c_pos = utilities.p_to_c(points[0])
-# print(Earth().ellipse_equation(c_pos[0], c_pos[1], c_pos[2]) )
-radars = []
-for point in points:
-    radar = Radar(point)
-    radars.append(radar)
-radar_system = RadarSystem(radars, Earth())
-
+radar_system = RadarSystem(10, Earth())
 ''' Test code'''
 current_time = 1
 for i in range(1000):
-    radar_system.try_detect_satellite(np.array([1000000, math.pi, math.pi]), current_time)
+    radar_system.update_radar_positions()
+    radar_system.try_detect_satellite(np.array([100000000, math.pi, math.pi]), current_time)
     current_time = current_time + 1
