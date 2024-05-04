e3991727cba85ac8d4a83f53aed42c74ef130123
andrequant <lorenzoandre10@gmail.com>
2024-05-04 01:35:00 +0100

    config implementation

--- Earth.py
+++ Earth.py
@@ -3,6 +3,7 @@ import math
 import matplotlib as plt
 
 import utilities as ut
+from config import config
 
 class Earth():
     def __init__(self):
@@ -11,10 +12,10 @@ class Earth():
         Properties of the Earth and its position.
         '''
 
-        self.re = 6378136.6
-        self.rp = 6356751.9
-        self.omega = 7.2921159 * 10**(-5)  
-        self.mass = 5.9722*10**24
+        self.re = config['earth']['major_axis']
+        self.rp = config['earth']['major_axis']
+        self.omega = config['earth']['omega']
+        self.mass = config['earth']['mass']
 
 
     def ellipse_equation(self, x, y, z):
@@ -112,16 +113,13 @@ class Earth():
         '''
         Atmosphere code, gets you the density.
         '''
-        R = 8.3144598
-        G = 9.80665
-        M = 0.028964425278793993
-        rl = {'Pb': [1.2250, 3.6392e-1, 1.9367e-1, 1.2165e-1, 7.4874e-2, 3.9466e-2, 0.01322, 3.8510e-3, 0.00143, 4.7526e-4, 0.00086, 2.8832e-4, 1.4934e-4, 0.000064, 2.3569e-5, 1.0387e-5, 4.3985e-6, 1.8119e-6, 7.4973e-7, 3.1593e-7, 1.4288e-7, 9.708e-8, 4.289e-8, 2.222e-8, 1.291e-8, 8.152e-9, 5.465e-9, 3.831e-9, 2.781e-9, 2.076e-9, 1.585e-9, 1.233e-9, 9.750e-10, 7.815e-30, 6.339e-10], 
-            'Tb': [288.15, 216.650, 216.650, 216.650,217.650, 221.650, 228.65, 251.050, 270.65, 256.650, 270.65, 245.450, 231.450, 214.65, 208.399, 198.639, 188.893, 186.87, 188.42, 195.08, 208.84, 240.00, 300, 360, 417.23, 469.27, 516.59, 559.63, 598.78, 634.39, 666.80, 696.29, 723.13, 747.57, 769.81], 
-            'hb': [0, 11e3, 15e3, 18e3, 21e3, 25e3, 32e3, 40e3, 47e3, 51e3, 56e3, 60e3, 65e3, 71e3, 75e3, 80e3, 85e3, 90e3, 95e3, 100e3, 105e3, 110e3, 115e3, 120e3, 125e3, 130e3, 135e3, 140e3, 145e3, 150e3, 155e3, 160e3, 165e3, 170e3, 175e3]}
-
+        R = config['atmosphere']['gas_const']
+        G = config['atmosphere']['gravity']
+        M = config['atmosphere']['molar_mass']
+        rl = config['atmosphere']['layers']
         layer = 0
         if distance > rl['hb'][-1]:
-             rho = 6e-10*np.exp(-(distance - 175e3)/29.5e3)    
+             rho = config['atmosphere']['rho_params']['rho0']*np.exp(-(distance - config['atmosphere']['rho_params']['rho1'])/config['atmosphere']['rho_params']['rho2'])    
         else:        
             for i in range(len(rl['hb']) - 1):
                 if distance >= rl['hb'][i] and distance < rl['hb'][i + 1]:
---
+++ config.py
@@ -0,0 +1,7 @@
+import json
+
+def load_config(file_path):
+    with open(file_path, 'r') as file:
+        return json.load(file)
+
+config = load_config('config.json')
