40f2d5314c45f03ab7d089cda4919f4d408f38d8
andrequant <lorenzoandre10@gmail.com>
2024-05-09 00:38:56 +0100

    Satellite Dynamics

--- Satellite.py
+++ Satellite.py
@@ -2,10 +2,14 @@ import numpy as np
 from Filter import LinearKalmanFilter
 from decorators import singleton
 
+from scipy.integrate import solve_ivp
+
 from config import config
 from ISimulator import ISimulator
 
 from SatelliteState import SatelliteState
+
+from config import config 
 
 from copy import deepcopy
 
@@ -24,6 +28,10 @@ class Satellite(ISimulator):
         observation_noise = np.diag(list(config['radar']['noise'].values())) #Check if the noise values in the radar (in the config file) are in the correct order
         self.filter = filter(prior_state.get_state(), prior_state.cov, None, observation_matrix, observation_noise, None)
         
+        self.mass = config['satellite']['mass']
+        self.area = config['satellite']['area']
+        self.drag = config['satellite']['drag_coefficient']
+        self.B = (self.area*self.drag)/self.mass
         
         self.earth = earth
 
@@ -34,6 +42,8 @@ class Satellite(ISimulator):
         self.recorded_true_states = [deepcopy(self.true_state)]
         self.recorded_estimated_states = [deepcopy(self.estimated_state)]
 
+
+    ##### Old
 
     def update_true_state(self):
         # Update the state one step.
@@ -54,6 +64,80 @@ class Satellite(ISimulator):
         # This vector should be passed through s_to_e and returned a new array of arrays in the earth coordinates
         pass
 
+    #######
+
+
+    ####### Actual Dymanics
+
+    def d_state(self, t, state):
+        # state in satelitte plane coordinates
+        # "t" is not required in the actual calculations, but it is neeeded
+        # for the scipy solver
+        # r -> Radial Distance
+        # v_r -> Radial Velocity
+        # phi -> Angle (axis of rotation of the plane)
+        # v_phi -> TANGENCIAL velocity (not angular velocity)
+
+        r, v_r, phi, v_phi = state
+
+        G = self.earth.G
+        Me = self.earth.mass
+        B = self.B
+
+        #rho = self.earth.air_density(plane_to_altitude( r, phi))
+        rho = 1
+
+
+        d_r = v_r
+        d_vr = - (G*Me/r**2) + (v_phi**2)/r
+        d_phi = v_phi/r
+        d_v_phi = -0.5*rho(r, phi) * v_phi**2 * B + (v_phi*v_r)/r
+
+        return np.array([d_r, d_vr, d_phi, d_v_phi])
+
+    def plane_to_altitude(self, r, phi):
+        earth_coor = convert_to_earth_coordinates(r, phi, self.plane_of_inclination)
+        return self.earth.distane_to_surface(earth_coor)
+
+
+    def crash(self, t, state):
+        #Check if satellite hitted the earth
+        r, v_r, phi, v_phi = state
+        return plane_to_altitude(r, phi)
+
+
+    def simulate(self, time_limit):
+        # Run the full integration
+
+        # it stops when the satellite crashes
+        # Setting the crash attributes
+        crash = self.crash
+        crash.terminal = True
+        crash.direction = -1 # Check if atitude is less than 0
+
+        # Time span for the solution
+        t_span = (0, 10)  # From t=0 to t=10
+        t_eval = np.linspace(t_span[0], t_span[1], 10000000)  # Grid. Arbritarially large
+
+        initial_state = convert_to_satellite_coordinates(
+            self.true_state.get_state())
+
+        method = config['sim_config']['solver']
+
+        sol = solve_ivp(self.ode_system, t_span, self.initial_conditions, method = method
+                        t_eval=t_eval, events=crash)
+        
+        # Use later outside this class, wherever the program will run
+        # Output results from the event
+        #if sol.t_events[0].size > 0:
+        #    print(f"Event occurred at t = {sol.t_events[0][0]}")
+        #    print(f"State at event: r = {sol.y_events[0][0][0]}, v_r = {sol.y_events[0][0][1]}")
+
+        return sol
+
+    #######
+
+
     
     def update_estimated_state(self, measurement=None):
         
@@ -72,7 +156,7 @@ class Satellite(ISimulator):
                     
         self.recorded_estimated_states.append(deepcopy(self.estimated_state))
 
-    def full_simulation(self):
+    def full_prediction(self):
         # Implement a loop to predict to full trajectory, given the current state.
         # It has to initialize a new Kalman Filter.
         # Check if it reached the ground.
