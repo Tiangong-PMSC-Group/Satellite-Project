5e236982e9ae11270b312b4567e7c5de5621e547
andrequant <lorenzoandre10@gmail.com>
2024-05-12 22:08:43 +0100

    tests

--- 3DVisual.py
+++ 3DVisual.py
@@ -1,9 +1,13 @@
+<<<<<<< Updated upstream
 import matplotlib
 matplotlib.use('TkAgg')
+=======
+>>>>>>> Stashed changes
 import plotly.graph_objects as go
 from Earth import Earth
 import numpy as np
 from ipywidgets import interact, IntSlider
+<<<<<<< Updated upstream
 import numpy as np
 import matplotlib.pyplot as plt
 from Earth import Earth
@@ -12,6 +16,17 @@ class VisualisationPlotly:
     def __init__(self, states1, states2):
         self.states1 = states1
         self.states2 = states2
+=======
+
+class VisualisationPlotly:
+    def __init__(self, x_positions, y_positions, z_positions, x_positions1, y_positions1, z_positions1):
+        self.x_positions = x_positions
+        self.y_positions = y_positions
+        self.z_positions = z_positions
+        self.x_positions1 = x_positions1
+        self.y_positions1 = y_positions1
+        self.z_positions1 = z_positions1
+>>>>>>> Stashed changes
         self.re = Earth().re
         self.rp = Earth().rp
         self.show_range = 8000000
@@ -24,6 +39,7 @@ class VisualisationPlotly:
         z = self.rp * np.outer(np.ones(np.size(u)), np.cos(v))
         return go.Surface(x=x, y=y, z=z, opacity=0.5, colorscale='Blues')
 
+<<<<<<< Updated upstream
     def create_trajectory(self, states, color, progress=100, opacity=1.0):
         progress_index = int(len(states) * progress / 100)
         x, y, z = zip(*states[:progress_index])
@@ -31,6 +47,14 @@ class VisualisationPlotly:
             x=x,
             y=y,
             z=z,
+=======
+    def create_trajectory(self, x, y, z, color, progress=100, opacity=1.0):
+        progress_index = int(len(x) * progress / 100)
+        return go.Scatter3d(
+            x=x[:progress_index],
+            y=y[:progress_index],
+            z=z[:progress_index],
+>>>>>>> Stashed changes
             mode='lines',
             line=dict(color=color, width=2),
             opacity=opacity  # Set the opacity for the trajectory
@@ -39,8 +63,13 @@ class VisualisationPlotly:
     def visualise(self, progress=100):
         fig = go.Figure()
         fig.add_trace(self.create_earth_surface())
+<<<<<<< Updated upstream
         fig.add_trace(self.create_trajectory(self.states1, 'red', progress, opacity=0.8))
         fig.add_trace(self.create_trajectory(self.states2, 'green', progress, opacity=0.6))
+=======
+        fig.add_trace(self.create_trajectory(self.x_positions, self.y_positions, self.z_positions, 'red', progress, opacity=0.8))
+        fig.add_trace(self.create_trajectory(self.x_positions1, self.y_positions1, self.z_positions1, 'green', progress, opacity=0.6))
+>>>>>>> Stashed changes
 
         fig.update_layout(
             scene=dict(
@@ -54,6 +83,7 @@ class VisualisationPlotly:
         fig.show()
 
 
+<<<<<<< Updated upstream
 orbit_radius = Earth().re + 1000000
 theta = np.linspace(0, 2 * np.pi, 100)
 states1 = [(orbit_radius * np.cos(t), orbit_radius * np.sin(t), 0) for t in theta]
@@ -77,3 +107,18 @@ plt.ylabel('X Coordinate')
 plt.legend()
 plt.grid(True)
 plt.show()
+=======
+orbit_radius = Earth().re + 1000000 
+theta = np.linspace(0, 2 * np.pi, 100)
+orbit_x = orbit_radius * np.cos(theta)
+orbit_y = orbit_radius * np.sin(theta)
+orbit_z = np.zeros_like(orbit_x)
+
+# Second trajectory
+orbit_x1 = orbit_radius * np.cos(theta)
+orbit_y1 = orbit_radius * np.sin(theta)
+orbit_z1 = np.zeros_like(orbit_x1)  
+
+visual_plotly = VisualisationPlotly(orbit_x, orbit_y, orbit_z, orbit_x1, orbit_y1, orbit_z1)
+interact(visual_plotly.visualise, progress=IntSlider(min=0, max=100, step=1, value=100, description='Progress'))
+>>>>>>> Stashed changes
