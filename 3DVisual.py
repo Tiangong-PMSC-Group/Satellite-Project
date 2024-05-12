117f060da8b7e4d59a4b73a9a538cc852ffb7640
575743278 <575743278@qq.com>
2024-05-12 22:34:50 +0100

    GUI works

--- 3DVisual.py
+++ 3DVisual.py
@@ -1,13 +1,12 @@
-<<<<<<< Updated upstream
 import matplotlib
+
+import utilities
+
 matplotlib.use('TkAgg')
-=======
->>>>>>> Stashed changes
 import plotly.graph_objects as go
 from Earth import Earth
 import numpy as np
 from ipywidgets import interact, IntSlider
-<<<<<<< Updated upstream
 import numpy as np
 import matplotlib.pyplot as plt
 from Earth import Earth
@@ -16,17 +15,6 @@ class VisualisationPlotly:
     def __init__(self, states1, states2):
         self.states1 = states1
         self.states2 = states2
-=======
-
-class VisualisationPlotly:
-    def __init__(self, x_positions, y_positions, z_positions, x_positions1, y_positions1, z_positions1):
-        self.x_positions = x_positions
-        self.y_positions = y_positions
-        self.z_positions = z_positions
-        self.x_positions1 = x_positions1
-        self.y_positions1 = y_positions1
-        self.z_positions1 = z_positions1
->>>>>>> Stashed changes
         self.re = Earth().re
         self.rp = Earth().rp
         self.show_range = 8000000
@@ -39,7 +27,6 @@ class VisualisationPlotly:
         z = self.rp * np.outer(np.ones(np.size(u)), np.cos(v))
         return go.Surface(x=x, y=y, z=z, opacity=0.5, colorscale='Blues')
 
-<<<<<<< Updated upstream
     def create_trajectory(self, states, color, progress=100, opacity=1.0):
         progress_index = int(len(states) * progress / 100)
         x, y, z = zip(*states[:progress_index])
@@ -47,14 +34,6 @@ class VisualisationPlotly:
             x=x,
             y=y,
             z=z,
-=======
-    def create_trajectory(self, x, y, z, color, progress=100, opacity=1.0):
-        progress_index = int(len(x) * progress / 100)
-        return go.Scatter3d(
-            x=x[:progress_index],
-            y=y[:progress_index],
-            z=z[:progress_index],
->>>>>>> Stashed changes
             mode='lines',
             line=dict(color=color, width=2),
             opacity=opacity  # Set the opacity for the trajectory
@@ -63,13 +42,8 @@ class VisualisationPlotly:
     def visualise(self, progress=100):
         fig = go.Figure()
         fig.add_trace(self.create_earth_surface())
-<<<<<<< Updated upstream
         fig.add_trace(self.create_trajectory(self.states1, 'red', progress, opacity=0.8))
         fig.add_trace(self.create_trajectory(self.states2, 'green', progress, opacity=0.6))
-=======
-        fig.add_trace(self.create_trajectory(self.x_positions, self.y_positions, self.z_positions, 'red', progress, opacity=0.8))
-        fig.add_trace(self.create_trajectory(self.x_positions1, self.y_positions1, self.z_positions1, 'green', progress, opacity=0.6))
->>>>>>> Stashed changes
 
         fig.update_layout(
             scene=dict(
@@ -83,42 +57,51 @@ class VisualisationPlotly:
         fig.show()
 
 
-<<<<<<< Updated upstream
 orbit_radius = Earth().re + 1000000
 theta = np.linspace(0, 2 * np.pi, 100)
 states1 = [(orbit_radius * np.cos(t), orbit_radius * np.sin(t), 0) for t in theta]
 states2 = [(orbit_radius * np.cos(t), orbit_radius * np.sin(t), 0) for t in theta]
 
+polar_real_state = []
+for state in states1:
+    polar_real_state.append(utilities.c_to_p(state))
+
+polar_predict_state = []
+for state in states2:
+    polar_predict_state.append(utilities.c_to_p(state))
 
 visual_plotly = VisualisationPlotly(states1, states2)
 interact(visual_plotly.visualise, progress=IntSlider(min=0, max=100, step=1, value=100, description='Progress'))
 
+def polar_plot(states1, states2):
 
-# plot figures
-x_coords1 = [state[0] for state in states1]
-x_coords2 = [state[0] for state in states2]
+    rho_from_states1 = [state[0] for state in states1]
+    polar_from_states1 = [state[2] for state in states1]
+    rho_from_states2 = [state[0] for state in states2]
+    polar_from_states2 = [state[2] for state in states2]
 
-plt.figure(figsize=(10, 5))
-plt.plot(theta, x_coords1, label='State 1 X Coordinate', marker='o')
-plt.plot(theta, x_coords2, label='State 2 X Coordinate', linestyle='--')
-plt.title('X Coordinates Over Time')
-plt.xlabel('Time (radians)')
-plt.ylabel('X Coordinate')
-plt.legend()
-plt.grid(True)
-plt.show()
-=======
-orbit_radius = Earth().re + 1000000 
-theta = np.linspace(0, 2 * np.pi, 100)
-orbit_x = orbit_radius * np.cos(theta)
-orbit_y = orbit_radius * np.sin(theta)
-orbit_z = np.zeros_like(orbit_x)
 
-# Second trajectory
-orbit_x1 = orbit_radius * np.cos(theta)
-orbit_y1 = orbit_radius * np.sin(theta)
-orbit_z1 = np.zeros_like(orbit_x1)  
+    fig, axs = plt.subplots(2, 1, figsize=(10, 9))
+    print(polar_from_states2)
 
-visual_plotly = VisualisationPlotly(orbit_x, orbit_y, orbit_z, orbit_x1, orbit_y1, orbit_z1)
-interact(visual_plotly.visualise, progress=IntSlider(min=0, max=100, step=1, value=100, description='Progress'))
->>>>>>> Stashed changes
+    axs[0].plot(range(len(states1)), rho_from_states1, label='Real Distance', marker='o')
+    axs[0].plot(range(len(states1)), rho_from_states2, label='Predicted Distance', linestyle='--')
+    axs[0].set_title('Distance Between Satellite And The Origin Of The Earth Over Time')
+    axs[0].set_xlabel('Time Steps')
+    axs[0].set_ylabel('Distance(m)')
+    axs[0].legend()
+    axs[0].grid(True)
+
+
+    axs[1].plot(range(len(states1)), polar_from_states1, label='Polar Of Real Trajactory', marker='o')
+    axs[1].plot(range(len(states1)), polar_from_states2, label='Polar Of Predicted Trajactory', linestyle='--')
+    axs[1].set_title('Polar Over Time')
+    axs[1].set_xlabel('Time Steps')
+    axs[1].set_ylabel('Polar')
+    axs[1].legend()
+    axs[1].grid(True)
+
+    plt.tight_layout()
+    plt.show()
+
+polar_plot(polar_real_state, polar_predict_state)
