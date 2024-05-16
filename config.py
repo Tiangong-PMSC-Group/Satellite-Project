91d709042f55cef3897c9dff5b92a4c39bfff672
575743278 <575743278@qq.com>
2024-05-16 11:06:38 +0100

    add docstring

--- config.py
+++ config.py
@@ -3,6 +3,15 @@ import os
 
 
 def load_config(file_path):
+    """
+       Load a JSON configuration file.
+
+       Args:
+           file_path (str): The path to the JSON configuration file.
+
+       Returns:
+           dict: The loaded configuration as a dictionary.
+       """
     with open(file_path, 'r') as file:
         return json.load(file)
 
@@ -15,9 +24,14 @@ path_in = os.path.join(os.path.dirname(__file__), 'configs/config_in.json')
 config_in = load_config(path_in)
 
 def save_config(data):
+    """
+       Save a dictionary to a JSON configuration file.
+
+       Args:
+           data (dict): The data to save to the JSON file.
+       """
     with open(path, 'w') as file:
         json.dump(data, file, indent=4)
 
-# config['radar']['noise']['rho'] = 99
-# save_config(config)
+
 
